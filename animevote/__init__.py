535a33133f4ac476ada5daa6292fca4b36cabe12
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 11:01:03 +0800

    add login to route

--- animevote/__init__.py
+++ animevote/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from animevote.route import index
+from animevote.route import index, login
 
 
 def create_app():
     app = Flask(__name__)
     app.add_url_rule('/', 'index', index)
     app.add_url_rule('/index', 'index', index)
+    app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
     return app
 
--- animevote/route.py
+++ animevote/route.py
@@ -4,3 +4,7 @@ from flask import render_template
 def index():
     posts = "test"
     return render_template('index.html', posts=posts)
+
+
+def login():
+    return render_template('login.html')

5b19ca7d22fc08293afc64b894192a98f6d086b5
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 15:10:13 +0800

    login manager initialization

--- animevote/__init__.py
+++ animevote/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from animevote.config import Config
 
 db = SQLAlchemy()  # db initialization
 migrate = Migrate()
+login = LoginManager()
 
 from animevote.route import index, login
 
@@ -14,7 +16,7 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)  # db initialization
     migrate.init_app(app, db)
-
+    login.init_app(app)
     app.add_url_rule('/', 'index', index)
     app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])

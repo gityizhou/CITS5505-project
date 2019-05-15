1c9088ee6b3ddab930f33da47345b413a34cebed
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 15:06:34 +0800

    add admin ,test user into user table

--- animevote/route.py
+++ animevote/route.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from animevote.forms import LoginForm
 from animevote.models import User, Poll
 
@@ -11,6 +11,9 @@ def index():
 def login():
     form = LoginForm(csrf_enabled=False)
     if form.validate_on_submit():
+        u = User.query.filter_by(username=form.username.data).first()
+        if u is None or not u.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
         return redirect(url_for('index'))
-
     return render_template('login.html', title="Sign In", form=form)

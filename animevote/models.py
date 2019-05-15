9b4f897355fb107cc7292a92e01bdef5948dd441
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 14:51:06 +0800

    add set_password,check_password method to User model

--- animevote/models.py
+++ animevote/models.py
@@ -1,4 +1,5 @@
 from animevote import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -11,6 +12,12 @@ class User(db.Model):
         return 'id={}, username={}, email={}, password_hash={}'.format(
             self.id, self.username, self.email, self.password_hash
         )
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Poll(db.Model):

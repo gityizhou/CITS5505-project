9383dcbaa5d1f361dc6493c502e988be6fa0ba8b
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 15:45:36 +0800

    create registerform in forms.py

--- animevote/forms.py
+++ animevote/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
+from animevote.models import User
+
 
 class LoginForm(FlaskForm):
     class Meta:
@@ -10,3 +12,22 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email Address", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Password Repeat", validators=[DataRequired(), EqualTo('password')])  # check password equal
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):          # check user duplicate
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('please use different username')
+
+    def validate_email(self, email):          # check mail duplicate
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('please use different email address')

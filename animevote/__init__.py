afbb69b7e96ef13ff1123084094b4c6b81db1818
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 12:09:19 +0800

    db migration (user table)

--- animevote/__init__.py
+++ animevote/__init__.py
@@ -1,23 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from animevote.route import index, login
+from animevote.config import Config
 
-
-db = SQLAlchemy()    # db initialization
+db = SQLAlchemy()  # db initialization
 migrate = Migrate()
+
+from animevote.route import index, login
 
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:animevote.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
-    db.init_app(app)     # db initialization
+    app.config.from_object(Config)
+    db.init_app(app)  # db initialization
     migrate.init_app(app, db)
 
     app.add_url_rule('/', 'index', index)
     app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
     return app
-
--- animevote/route.py
+++ animevote/route.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for
 from animevote.forms import LoginForm
+from animevote.models import User
 
 
 def index():

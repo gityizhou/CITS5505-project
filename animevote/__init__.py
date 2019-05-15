0329dcee66bbd2026ffccb97ffdf0ece995e5889
Yi Zhou <zhouyi119119@gmail.com>
2019-05-15 11:26:33 +0800

    db initialization

--- animevote/__init__.py
+++ animevote/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from animevote.route import index, login
+
+
+db = SQLAlchemy()    # db initialization
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:animevote.db"
+    db.init_app(app)     # db initialization
+    migrate.init_app(app, db)
+
     app.add_url_rule('/', 'index', index)
     app.add_url_rule('/index', 'index', index)
     app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
